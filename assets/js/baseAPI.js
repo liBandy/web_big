98dc0ad73178623b09b7fac7fb4a9632ea549b6b
andy <584755025@qq.com>
2023-09-26 10:04:14 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,16 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://127.0.0.1:3007' + options.url
     console.log(options.url);
-
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function(res) {
+        // console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/04/big/login.html'
+        }
+    }
 })
